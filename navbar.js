4f6a235663e731fc4942fdfe79fdef108ec75636
andrzej <andreq114@gmail.com>
2019-03-15 12:00:05 +0100

    kopia

--- navbar.js
+++ navbar.js
@@ -1,6 +1,7 @@
 let nav =
 '<nav><a class="nav-link active" href="index.html">Home</a>'+
 '<a class="nav-link" href="#">Blog</a>'+
+'<a class="nav-link" href="quiz.html">Quiz</a>'+
 '<a class="nav-link" href="contact.html">Kontakt</a></nav>';
 
 let elem =document.querySelector(".navbar");
